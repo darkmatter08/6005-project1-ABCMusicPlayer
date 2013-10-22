eda0261860ad66e8ac4122b5f1abdcf32f6d6816
dalitso banda <dbanda@mit.edu>
2013-10-22 13:08:51 -0400

    included aSt test and int pair class to store paired ints

---
+++ src/sound/IntPair.java
@@ -0,0 +1,12 @@
+package sound;
+
+public class IntPair {
+	final int numerator;
+    final int denominator;
+    
+    public IntPair(int num, int denom){
+    	this.denominator = denom;
+    	this.numerator = num;
+    }
+
+}
