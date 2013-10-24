a56a64c4e2cdbfecd4253211421e86b7536185bc
dalitso banda <dbanda@mit.edu>
2013-10-24 16:32:36 -0400

    added set/get methods to piece

--- src/sound/Piece.java
+++ src/sound/Piece.java
@@ -10,23 +10,45 @@ public class Piece implements Sequence{
 	private final String title;
 	
 	// not sure if we need to keep some of these fields in the ast
-	private final String composer = null;
-	private final IntPair meter = null;
-	private final IntPair defaultLength = null;
+	private String composer;
+	private IntPair meter;
 	private final int tempo;
 	private final int indexNumber;
-	private final KeySignature keySig; //Key is an enum 
+	private final KeySignature keySig; //KeySignature is an enum 
 	private IntPair shortestLength;
-
+	private IntPair defaultLength;
+	
 	public Piece(KeySignature keySig, int indexNumber, int tempo, String title) {
 		this.keySig = keySig;
 		this.indexNumber = indexNumber;
 		this.tempo = tempo;
 		this.title = title;
 		this.measures = new ArrayList<Measure>();
-
 	}
-	
+	public void setDefaultLength(IntPair defaultLength){
+		this.defaultLength = defaultLength;
+	}
+	public IntPair getDefaultLength(){
+		return this.defaultLength;
+	}
+	public void setComposer(String composer){
+		this.composer = composer;
+	}
+	public String getComposer(){
+		return this.composer;
+	}
+	public void setMeter(IntPair meter){
+		this.meter = meter;
+	}
+	public IntPair getMeter(){
+		return this.meter;
+	}
+	public int getIndexNumber(){
+		return this.indexNumber;
+	}
+	public String getTitle(){
+		return this.title;
+	}
 	public int getTempo(){
 		return this.tempo;
 	}
@@ -43,7 +65,7 @@ public class Piece implements Sequence{
 		// This will take a measure and add it to the measures list.  
 	}
 	public KeySignature getKeySig(){
-		return this.keySig;//This get method will be used to get the keySig attibute
+		return this.keySig;//This get method will be used to get the keySig attribute
 	}
 	@Override
 	public List<MusicalAtom> getSequence(){
@@ -58,16 +80,12 @@ public class Piece implements Sequence{
 	@Override
 	public IntPair getShortestLength() {
 		// TODO Auto-generated method stub
-		return this.shortestLength;
-		
+		return this.shortestLength;	
 	}
-	
 	@Override
 	public String toString(){
 		List<MusicalAtom> sequence = this.getSequence();
 		return sequence.toString();
 	}
-
-
 }
  
