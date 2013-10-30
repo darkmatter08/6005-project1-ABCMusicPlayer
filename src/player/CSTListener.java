fc479469733382afb9b4c05f7b3f7aabbd7c43a3
Luis Sanmiguel <luiss@mit.edu>
2013-10-30 04:08:39 -0400

    Fixed defaultLength and other minor bugs

--- src/player/CSTListener.java
+++ src/player/CSTListener.java
@@ -41,11 +41,12 @@ import grammar.ABCMusicParserListener;
 import player.LyricsListenerHelper;
 public class CSTListener implements ABCMusicParserListener{
 	Piece piece;
-	
+	IntPair defaultNoteLength;
 	Queue<String> voices = new LinkedList<String>() ;
 	Queue<String> notes = new LinkedList<String>();
 	Queue<String> lyrics = new LinkedList<String>();
 	Queue<Object> measureAtoms = new LinkedList<Object>();
+	
 	@Override
 	public void enterEveryRule(ParserRuleContext arg0) {
 		//System.out.println("Entering: "+ arg0.getClass().toString() + '\n' + arg0.getText());
@@ -78,6 +79,10 @@ public class CSTListener implements ABCMusicParserListener{
 		if (ctx.BARLINE() != null){
 			System.out.println("Added a barline to measure atoms:"+ ctx.getText());
 			measureAtoms.add(ctx.BARLINE().getText());
+		}
+		if (ctx.NTHREPEAT() != null){
+			System.out.println("Added a repeat to measure atoms:"+ ctx.getText());
+			measureAtoms.add(ctx.NTHREPEAT().getText());
 		}
 	}
 
@@ -130,11 +135,11 @@ public class CSTListener implements ABCMusicParserListener{
 
 	@Override
 	public void enterMidtunefield(MidtunefieldContext ctx) {
-		// TODO Auto-generated method stub
-		String voiceStr = ctx.getText().replace("\n", "").substring(2);
-		voices.add(voiceStr);
-		System.out.println("added to voices quoe ->" + voiceStr);
-		
+		if (ctx.VOICEBODY() != null) {
+			String voiceStr = ctx.VOICEBODY().getText().replace("\n", "").substring(2);
+			voices.add(voiceStr);
+			System.out.println("added to voices queue ->" + voiceStr);
+		}
 	}
 
 	@Override
@@ -171,7 +176,7 @@ public class CSTListener implements ABCMusicParserListener{
 	@Override
 	public void enterAbcheader(AbcheaderContext ctx) {
 		// @cr we will need to make sure the grammar handles whitespace correctly in these fields
-		String title = ctx.TITLE().toString().replace("\n", "");
+		String title = ctx.TITLE().toString().substring(2).replace("\n", "");
 		KeySignature Key = KeySignature.valueOf(ctx.KEY().toString().substring(2).replace("\n", ""));
 		
 		// set the tempo to 100 as default. Change if specified. 
@@ -189,15 +194,22 @@ public class CSTListener implements ABCMusicParserListener{
 			meter = new IntPair(num, denom);
 		}
 		
-		// set the default length to 1/1 as default. Change if specified.
-		IntPair defaultLength = new IntPair(meter.numerator, meter.denominator);
 		if (ctx.optionalfields().LENGTH() != null){
 			int num = Integer.parseInt(ctx.optionalfields().LENGTH().get(0).toString().substring(2,3));
 			int denom = Integer.parseInt(ctx.optionalfields().LENGTH().get(0).toString().substring(4,5));
-			defaultLength = new IntPair(num, denom);
+			this.defaultNoteLength = new IntPair(num, denom);
+			this.piece = new Piece(Key, tempo, title, meter, this.defaultNoteLength);
+		}
+		else{
+			if (meter.getValue() < 0.75){
+				this.defaultNoteLength = new IntPair(1,16);
+			}
+			else{
+				this.defaultNoteLength = new IntPair(1,8);
+			}
+			this.piece = new Piece(Key, tempo, title, meter, this.defaultNoteLength);
 		}
 		
-		this.piece = new Piece(Key, tempo, title, meter, defaultLength);
 		
 	}
 
@@ -215,36 +227,36 @@ public class CSTListener implements ABCMusicParserListener{
 
 	@Override
 	public void enterBodysection(BodysectionContext ctx) {
-		// TODO Auto-generated method stub
 		System.out.println("scanning lyrics");
 		if (ctx.LYRIC() != null){
 			for (String lyric : LyricsListenerHelper.breakLyrics(ctx.LYRIC().getText().substring(2))){
 				this.lyrics.add(lyric);
 			}
-			System.out.println("put thes lyrics on the stack:" + LyricsListenerHelper.breakLyrics(ctx.LYRIC().getText().toString().substring(2)));
+			System.out.println("put the lyrics on the stack:" + LyricsListenerHelper.breakLyrics(ctx.LYRIC().getText().toString().substring(2)));
 		}
 	}
 
 	@Override
 	public void exitBodysection(BodysectionContext ctx) {
-		// TODO Auto-generated method stub
+
 		// Ideally the stacks should be processed here and new the bulk of the AST building performed
 		Measure m = new Measure();
 		String lyric = null;
 		boolean holdLyrics = false;
 		if (this.lyrics.isEmpty()){holdLyrics = true;}
-		System.out.println("size" + measureAtoms.size()+ "contents" + measureAtoms.toString());
+		System.out.println("size: " + measureAtoms.size()+ "contents: " + measureAtoms.toString());
 		while (!(this.measureAtoms.isEmpty())){
 			Object atom = this.measureAtoms.poll();
 			if (!(this.lyrics.isEmpty())){
 				lyric = this.lyrics.poll();
+				System.out.println("Lyric polled off the queue: "+lyric);
 				if (lyric.equals("|")){
 					holdLyrics = true;
 				}
 			}
-			if (atom.equals("|") || atom.equals("||") 
-					|| atom.equals("[|") || atom.equals( "|]") ||
-							atom.equals(":|") || atom.equals("|:")){
+			if (atom.toString().equals("|") || atom.toString().equals("||") 
+					|| atom.toString().equals("[|") || atom.toString().equals( "|]") ||
+							atom.toString().equals(":|") || atom.toString().equals("|:")){
 				if (!(m.isEmpty())){
 					System.out.println("adding this measure to piece" + m);
 					this.piece.addMeasure(m);
@@ -253,6 +265,7 @@ public class CSTListener implements ABCMusicParserListener{
 			}
 			else{
 				Chord c = (Chord) atom;
+				System.out.println("Chord to be paired with lyric: " + c.toString());
 				if (holdLyrics == false){
 					c.addLyrics(lyric); 
 				}
@@ -309,11 +322,10 @@ public class CSTListener implements ABCMusicParserListener{
 			IntPair length;
 			if (ctx.note().notelength().getChildCount() != 0){
 				String expression = ctx.note().notelength().getText();
-				System.out.println("First: "+expression);
 				length = LyricsListenerHelper.noteGetPair(expression);
 			}
 			else{
-				length = new IntPair(1,1);
+				length = this.defaultNoteLength;
 			}
 			
 			if (ctx.note().noteorrest() != null){
